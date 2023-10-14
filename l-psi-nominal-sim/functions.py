5ee3604f83fb1b078ea48f8c7a30da359836233c
Ronit <73128348+ronitchitre@users.noreply.github.com>
2023-10-15 02:17:22 +0530

    nominal controller simulation

---
+++ l-psi-nominal-sim/functions.py
@@ -0,0 +1,98 @@
+import numpy as np
+import constants
+
+def l_psi_dynamics(state, u, state_L, state_L_dot):
+    l = state[0]
+    psi = state[1]
+    theta_F = state[2]
+    v_F = state[3]
+
+    theta_L = state_L[2]
+    v_L = state_L[3]
+    theta_L_dot = state_L_dot[2]
+
+    gamma = psi + theta_L - theta_F
+
+    l_dot = v_F*np.cos(gamma) - v_L*np.cos(psi)
+    psi_dot = (v_L*np.sin(psi) - v_F*np.sin(gamma) - theta_L_dot*l) / (l)
+    theta_F_dot = (v_F * u[0] / constants.L)
+    v_F_dot = u[1]
+
+    return np.array([l_dot, psi_dot, theta_F_dot, v_F_dot])
+
+def flat_to_nonlinear(y, y_dot, state_L, state_L_dot):
+    theta_L = state_L[2]
+    x_L1_dot = state_L_dot[1]
+    x_L2_dot = state_L_dot[2]
+
+    l = np.linalg.norm(y)
+    psi = np.pi + np.arctan2(y[1], y[0]) - theta_L
+    theta_F = np.arctan2((y_dot[1] + x_L2_dot), (y_dot[0], x_L1_dot))
+    v_F = ((y_dot[0] + x_L1_dot)**2 + (y_dot[1] + x_L2_dot)**2)**0.5
+    return np.array([l, psi, theta_F, v_F])
+
+def nonlinear_to_flat(state, state_L):
+    l = state[0]
+    psi = state[1]
+    theta_L = state_L[2]
+
+    y_1 = l*np.cos(psi+ theta_L - np.pi)
+    y_2 = l*np.sin(psi + theta_L - np.pi)
+
+    return np.array([y_1, y_2])
+
+def flat_system_dynamics(z, v):
+    y = z[0:2]
+    y_dot = z[2:4]
+
+    z1_dot = y_dot
+    z2_dot = v
+
+    return np.concatenate([z1_dot, z2_dot])
+
+def pd_controller(z, z_des):
+    y = z[0:2]
+    y_dot = z[2:4]
+
+    y_des = z_des[0:2]
+    y_dot_des = z_des[2:4]
+
+    v = constants.Kp*(y_des - y) + constants.Kd*(y_dot_des - y_dot)
+    return v
+
+def flat_control_to_real_control(v, state, x_L_ddot):
+    theta_F = state[2]
+    v_F = state[3]
+
+    x_L1_dot_dot = x_L_ddot[0]
+    x_L2_dot_dot = x_L_ddot[1]
+
+    v_1 = v[0]
+    v_2 = v[1]
+
+    u_1 = (constants.L / v_F**2) * (-1*(v_1 + x_L1_dot_dot)*np.sin(theta_F) + (v_2 + x_L2_dot_dot)*np.cos(theta_F))
+    u_2 = (v_1 + x_L1_dot_dot)*np.cos(theta_F) + (v_2 + x_L2_dot_dot)*np.sin(theta_F)
+
+    return np.array([u_1, u_2])
+
+def find_flat_traj(des_traj, state_L, state_L_dot):
+    l_d = des_traj[0]
+    psi_d = des_traj[1]
+
+    theta_L = state_L[2]
+
+    l_d_dot = des_traj[2]
+    psi_dot = des_traj[3]
+
+    theta_L_dot = state_L_dot[2]
+
+    y_d1 = l_d * np.cos(psi_d + theta_L - np.pi)
+    y_d2 = l_d * np.sin(psi_d + theta_L - np.pi)
+
+    A = np.array([[y_d1, -1*y_d2], [y_d2, y_d1]])
+    b = np.array([l_d_dot/l_d, psi_dot + theta_L_dot])
+    y_d1_dot, y_d2_dot = A.dot(b)
+
+    return np.array([y_d1, y_d2, y_d1_dot, y_d2_dot])
+
+
--- l-psi-nominal-sim/main.py
+++ l-psi-nominal-sim/main.py
@@ -1,35 +1,99 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import constants
+import functions
+from scipy.integrate import odeint
 
-def l_psi_dynamics(state_F, u, state_L, state_L_dot):
-    l = state_F[0]
-    psi = state_F[1]
-    theta_F = state_F[2]
-    v_F = state_F[3]
+tf = 20
+N = 1000
 
-    theta_L = state_L[2]
-    v_L = state_L[3]
-    theta_L_dot = state_L_dot[2]
+#initial position, heading and velocity of follower
+x_F1 = 0
+x_F2 = 0
+theta_F = np.pi / 2
+v_F = 1
 
-    gamma = psi + theta_L - theta_F
+# leader trajectory
+def x_L1_traj(t):
+    return 1
+def x_L1_dot_traj(t):
+    return 0
+def x_L1_ddot_traj(t):
+    return 0
+def x_L2_traj(t):
+    return 1 + t
+def x_L2_dot_traj(t):
+    return 1
+def x_L2_ddot_traj(t):
+    return 0
+def theta_L_traj(t):
+    return np.pi/2
+def theta_L_dot_traj(t):
+    return 0
+def v_L_traj(t):
+    return 1
+def v_L_dot_traj(t):
+    return 0
 
-    l_dot = v_F*np.cos(gamma) - v_L*np.cos(psi)
-    psi_dot = (v_L*np.sin(psi) - v_F*np.sin(gamma) - theta_L_dot*l) / (l)
-    theta_F_dot = (v_F * u[0] / constants.L)
-    v_F_dot = u[1]
+#initial flat output
+y1 = x_F1 - x_L1_dot_traj(0)
+y2 = x_F2 - x_L2_dot_traj(0)
+y1_dot = v_F*np.cos(theta_F) - x_L1_dot_traj(0)
+y2_dot = v_F*np.sin(theta_F) - x_L2_dot_traj(0)
+z = np.array([y1, y2, y1_dot, y2_dot])
 
-    return np.array([l_dot, psi_dot, theta_F_dot, v_F_dot])
+#initial state
+l = ((x_F1 - x_L1_traj(0))**2 + (x_F2 - x_L2_traj(0))**2)**0.5
+psi = np.pi + np.arctan2(x_L2_traj(0) - x_F2, x_L1_traj(0) - x_F1) - theta_L_traj(0)
+state_ic = np.array([l, psi, theta_F, v_F])
 
-def flat_to_nonlinear(y, y_dot, state_L, state_L_dot):
-    theta_L = state_L[2]
-    x_L1_dot = state_L_dot[1]
-    x_L2_dot = state_L_dot[2]
+#desired output
+def l_des(time):
+    return 1
+def psi_des(time):
+    return 0
+def l_dot_des(time):
+    return 0
+def psi_dot_des(time):
+    return 0
 
-    l = np.linalg.norm(y)
-    psi = np.pi + np.arctan2(y[1], y[0]) - theta_L
-    theta_F = np.arctan2((y_dot[1] + x_L2_dot), (y_dot[0], x_L1_dot))
-    v_F = ((y_dot[0] + x_L1_dot)**2 + (y_dot[1] + x_L2_dot)**2)**0.5
-    return np.array([l, psi, theta_F, v_F])
+#desired output flat_space
+def z_des_traj(time):
+    y1_des = l_des(time)*np.cos(psi_des(time) + theta_L_traj(time) - np.pi)
+    y2_des = l_des(time)*np.sin(psi_des(time) + theta_L_traj(time) - np.pi)
+    b1 = l_dot_des(time) / l_des(time)
+    b2 = psi_dot_des(time) + theta_L_dot_traj(time)
+    y1_dot_des =  y1_des*b1 - y2_des*b2
+    y2_dot_des = y2_des*b1 + y1_des*b2
+    return np.array([y1_des, y2_des, y1_dot_des, y2_dot_des])
 
+def system_dynamics_flat(z, t):
+    z_des = z_des_traj(t)
+    v = functions.pd_controller(z, z_des)
+    z_dot = functions.flat_system_dynamics(z, v)
+    return z_dot
+
+time = np.linspace(0, tf, N)
+z_traj = odeint(system_dynamics_flat, z, time)
+
+def v_traj(time):
+    z_des = z_des_traj(time)
+    i = (N - 1)*time / tf #check if correct
+    z = z_traj[i]
+    return functions.pd_controller(z, z_des)
+
+def system_dynamics_nh(state, time):
+    v = v_traj(time)
+    x_L_ddot = np.array([x_L1_ddot_traj(time), x_L2_ddot_traj(time)])
+    u = functions.flat_control_to_real_control(v, state, x_L_ddot)
+    state_dot = functions.l_psi_dynamics(state, u)
+
+state_traj = odeint(system_dynamics_nh, state_ic, time)
+
+state_traj_proj = np.zeros_like(state_traj)
+for t in time:
+    state_L = np.array([x_L1_traj(t), x_L2_traj(t), theta_L_traj(t), v_L_traj(t)])
+    state_L_dot = np.array([x_L1_dot_traj(t), x_L2_dot_traj(t), theta_L_dot_traj(t), v_L_dot_traj(t)])
+    i = (N - 1)*time / tf
+    z = z_traj[i]
+    state_traj_proj[i] = functions.flat_to_nonlinear(z[0:2], z[2:4], state_L, state_L_dot)
 
